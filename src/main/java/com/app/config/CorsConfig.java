b93209350a60629bcdf672a5d14b2b296f185ed6
mulanisam <mulanisam55@gmail.com>
2024-08-03 00:04:30 +0530

    cors  update

--- src/main/java/com/app/config/CorsConfig.java
+++ src/main/java/com/app/config/CorsConfig.java
@@ -16,10 +16,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                		.allowedOrigins("http://localhost:3000","https://sohel-chicken.vercel.app","https://sohel-chicken-samirs-projects-9ab2b522.vercel.app","http://sohel-chicken.vercel.app") 
-                        .allowedMethods("GET", "POST", "PUT", "DELETE").
-                        allowedHeaders("Access-Control-Allow-Origin", "Origin")
-                        .allowedOrigins("*");
+                		.allowedOrigins("*") 
+                        .allowedMethods("*").
+                        allowedHeaders("*");
+
             }
         };
     }
