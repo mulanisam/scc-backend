b94fb1355dbddc87c71ab20c3ae4185e344e007d
Samir Mulani <mulanisam55@gmail.com>
2025-09-26 21:57:20 +0530

    properties changes

--- src/main/java/com/app/config/CorsConfig.java
+++ src/main/java/com/app/config/CorsConfig.java
@@ -4,6 +4,7 @@ package com.app.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -12,6 +13,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+	@Value("${frontend.url}")
+	private String frontendurl;
+	
     private static final Logger logger = LoggerFactory.getLogger(CorsConfig.class);
 
     @Bean
@@ -20,9 +24,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                            "*"
-                        ) // Restrict to trusted domains
+                        .allowedOrigins(frontendurl) // Restrict to trusted domains
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH") // Include OPTIONS and PATCH if needed
                         .allowedHeaders("*") // Restrict headers if possible
                         .exposedHeaders("*") // Expose additional headers if needed
